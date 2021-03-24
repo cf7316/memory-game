ba9442641307836ed2939940af4e42e0761ea6c1
cf7316 <59989514+cf7316@users.noreply.github.com>
2021-03-24 17:15:26 -0400

    Sound Player

--- script.js
+++ script.js
@@ -110,6 +110,10 @@ function loseGame(){
 
 function winGame(){
   stopGame();
+  (new SoundPlayer(context)).play(392, 0.5, "sine").stop(1.25);
+  (new SoundPlayer(context)).play(262, 0.5, "sine", 0.3).stop(1);
+  (new SoundPlayer(context)).play(330, 0.5, "sine", 0.4).stop(0.75);
+  (new SoundPlayer(context)).play(523, 0.5, "sine", 0.6).stop(1.5);
   alert("Game Over. You won!");
 }
 
@@ -142,3 +146,74 @@ function guess(btn) {
     loseGame();
   }
 }
+
+//SoundPlayer
+// Original JavaScript code by Chirp Internet: chirpinternet.eu
+// Please acknowledge use of this code by including this header.
+
+function SoundPlayer(audioContext, filterNode) {
+  this.audioCtx = audioContext;
+  this.gainNode = this.audioCtx.createGain();
+  if(filterNode) {
+    // run output through extra filter (already connected to audioContext)
+    this.gainNode.connect(filterNode);
+  } else {
+    this.gainNode.connect(this.audioCtx.destination);
+  }
+  this.oscillator = null;
+}
+
+SoundPlayer.prototype.setFrequency = function(val, when) {
+  if(this.oscillator !== null) {
+    if(when) {
+      this.oscillator.frequency.setValueAtTime(val, this.audioCtx.currentTime + when);
+    } else {
+      this.oscillator.frequency.setValueAtTime(val, this.audioCtx.currentTime);
+    }
+  }
+  return this;
+};
+
+SoundPlayer.prototype.setVolume = function(val, when) {
+  if(when) {
+    this.gainNode.gain.exponentialRampToValueAtTime(val, this.audioCtx.currentTime + when);
+  } else {
+    this.gainNode.gain.setValueAtTime(val, this.audioCtx.currentTime);
+  }
+  return this;
+};
+
+SoundPlayer.prototype.setWaveType = function(waveType) {
+  this.oscillator.type = waveType;
+  return this;
+};
+
+SoundPlayer.prototype.play = function(freq, vol, wave, when) {
+  this.oscillator = this.audioCtx.createOscillator();
+  this.oscillator.connect(this.gainNode);
+  this.setFrequency(freq);
+  if(wave) {
+    this.setWaveType(wave);
+  }
+  this.setVolume(1/1000);
+  if(when) {
+    this.setVolume(1/1000, when - 0.02);
+    this.oscillator.start(when - 0.02);
+    this.setVolume(vol, when);
+  } else {
+    this.oscillator.start();
+    this.setVolume(vol, 0.02);
+  }
+  return this;
+};
+
+SoundPlayer.prototype.stop = function(when) {
+  if(when) {
+    this.gainNode.gain.setTargetAtTime(1/1000, this.audioCtx.currentTime + when - 0.05, 0.02);
+    this.oscillator.stop(this.audioCtx.currentTime + when);
+  } else {
+    this.gainNode.gain.setTargetAtTime(1/1000, this.audioCtx.currentTime, 0.02);
+    this.oscillator.stop(this.audioCtx.currentTime + 0.05);
+  }
+  return this;
+};
